948050ddc6f3ac843472553138f006f6ecc4e7a1
忽如寄 <huruji3@foxmail.com>
2024-08-12 17:12:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,10 @@ const fs = require('fs')
 const path = require('path')
 const select = require('puppeteer-select')
 
-const wsurl = 'ws://127.0.0.1:9222/devtools/browser/3cbd922e-c2a1-47bb-a3eb-aa5c0a875a84'
+const wsurl = 'ws://127.0.0.1:9222/devtools/browser/87101bfe-4126-4a9f-aec7-ee9f116a8268'
 // const url2 = "https://tcs.bytedance.net/workprocess/6971247424669024771?mode=scan&task_ids=7173256439748641293&submit=directly";
 // const url3 = 'https://tcs.bytedance.net/workprocess/6677808795546026509/?mode=scan&task_ids=7180498975613518371&submit=directly'
-const url = 'https://github.com/replicate/replicate-javascript/network/dependents'
+const url = 'https://github.com/sindresorhus/capture-website/network/dependents'
 
 const sleep = async (time) => {
   return new Promise((resolve) => {
@@ -34,9 +34,21 @@ const sleep = async (time) => {
       let pageNum = 1
       while (shouldContinue) {
         console.log(`开始第${pageNum}页数据`)
-        const elHandleArray = await page.$$eval('#dependents .Box-row a.text-bold',els => els.map(el => el.href))
+        const items = await page.$$eval('#dependents .Box-row', els => {
+          return els.map(el => {
+            return {
+              url: el.querySelector('a.text-bold').href.trim(),
+              star: el.querySelector('span.color-fg-muted.text-bold.pl-3').textContent.trim(),
+              fork: el.querySelectorAll('span.color-fg-muted.text-bold.pl-3')[1].textContent.trim(),
+            }
+          })
+        })
+        const itemsContent = items.map(item => {
+          return `${item.url}  ${item.star}  ${item.fork}`
+        })
+        // const elHandleArray = await page.$$eval('#dependents .Box-row a.text-bold',els => els.map(el => el.href))
         const content = fs.readFileSync(filePath).toString().split('\n');
-        const newContent = Array.from(new Set([...content, ...elHandleArray])).filter(n => n).join('\n')
+        const newContent = Array.from(new Set([...content, ...itemsContent])).filter(n => n).join('\n')
         fs.writeFileSync(filePath, newContent)
         const hasNext = await page.$eval(nextSelector, (ele) => {
           return !ele.disabled
@@ -47,11 +59,30 @@ const sleep = async (time) => {
           shouldContinue = false
         }
         const nextBtn = await page.$(nextSelector)
-        nextBtn.click()
-        console.log('next button clicked')
+        try {
+          nextBtn.click()
+          console.log('next button clicked')
+        } catch(err) {
+          console.log(err)
+        }
         await sleep(10)
         pageNum++
       }
+      // 最后根据 star 数重排
+      const content = fs.readFileSync(filePath).toString().split('\n');
+      const info = content.map((item) => {
+        console.log('info: ', item)
+        const lineInfo = item.split(/\s+/)
+        return {
+          url: lineInfo[0],
+          star: lineInfo[1],
+          fork: lineInfo[2]
+        }
+      }).sort((a, b) => b.star - a.star).map(item => {
+        return `${item.url}  ${item.star}  ${item.fork}`
+      }).join('\n')
+      fs.writeFileSync(filePath, info)
+
       console.log('task finished')
     } catch (err) {
       console.error(err)
